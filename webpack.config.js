d83a0386627f0de231e958c0a7f22fabac429c37
NAY3950 <pramodnayak@northwesternmutual.com>
2018-12-24 08:35:31 -0600

    Adding files to Git

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: path.join(__dirname, '/bundle'),
+    filename: 'index_bundle.js'
+  },
+  devServer: {
+    inline: true,
+    port: 9093
+  },
+  node: {
+    fs: 'empty'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  externals: {
+    'Config': JSON.stringify(require('./config/development.json'))
+  }
+}
